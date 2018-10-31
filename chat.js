7474a323de4d18adc873f3f74f5de848935c0167
alvinwicaksono <alvin.wicaksono@ti.ukdw.ac.id>
2018-10-31 11:51:06 +0700

    Enkrip pass, tambah module mysql, database sudah connect,query blm bikin

--- chat.js
+++ chat.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const port = 3000;
+
+var connection = mysql.createConnection({
+	//properties
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'ngomongo'
+});
+
+//cek koneksi database
+connection.connect(function(error){
+	if(!!error){
+		console.log("error");
+	}else {
+		console.log("Database connected");
+	}
+})
 
 server = app.listen(port, function(){
 	console.log('Server aktif di port '+port);
@@ -24,10 +42,10 @@ io.on('connect', (socket) => {
 		console.log(data.nama);
 		console.log(data.isi);
 		//io.sockets.emit('kirim_pesan', {isi: data.isi, nama: data.nama});
-		
+
 		//mengirim ke client tertentu berdasarkan socket id
 		//caranya menggunakan database untuk menyimpan id
 		io.sockets.connected[client[0]].emit('kirim_pesan', {isi: data.isi, nama: data.nama});
 		io.sockets.connected[client[1]].emit('kirim_pesan', {isi: data.isi, nama: data.nama});
-	}); 
+	});
 });
